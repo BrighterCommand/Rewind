f6e40f01c223a135eb4edc8415557783756fa957
Ian Cooper <ian.cooper@justeattakeaway.com>
2023-10-21 14:22:58 +0100

    sort a toc

--- internal/pages/sort_toc_test.go
+++ internal/pages/sort_toc_test.go
@@ -62,16 +62,35 @@ func TestToSectionSort(t *testing.T) {
 	//assert
 	if sortedTOC[0].Name != "SectionOne" {
 		t.Errorf("Expected SectionOne to be first, got %s", sortedTOC[0].Name)
+	} else {
+		if sortedTOC[0].Section.Entries[0].Name != "DocumentFour" {
+			t.Errorf("Expected DocumentFour to be first, got %s", sortedTOC[0].Section.Entries[0].Name)
+		}
+		if sortedTOC[0].Section.Entries[1].Name != "DocumentThree" {
+			t.Errorf("Expected DocumentThree to be second, got %s", sortedTOC[0].Section.Entries[1].Name)
+		}
 	}
 
 	if sortedTOC[1].Name != "SectionTwo" {
 		t.Errorf("Expected SectionTwo to be second, got %s", sortedTOC[1].Name)
+	} else {
+		if sortedTOC[1].Section.Entries[0].Name != "DocumentTwo" {
+			t.Errorf("Expected DocumentTwo to be first, got %s", sortedTOC[1].Section.Entries[0].Name)
+		}
+		if sortedTOC[1].Section.Entries[1].Name != "DocumentOne" {
+			t.Errorf("Expected DocumentOne to be second, got %s", sortedTOC[1].Section.Entries[1].Name)
+		}
 	}
 
 	if sortedTOC[2].Name != "SectionThree" {
 		t.Errorf("Expected SectionThree to be third, got %s", sortedTOC[2].Name)
+	} else {
+		if sortedTOC[2].Section.Entries[0].Name != "DocumentFive" {
+			t.Errorf("Expected DocumentFive to be first, got %s", sortedTOC[2].Section.Entries[0].Name)
+		}
+		if sortedTOC[2].Section.Entries[1].Name != "DocumentSix" {
+			t.Errorf("Expected DocumentSix to be second, got %s", sortedTOC[2].Section.Entries[1].Name)
+		}
 	}
-
-	//ToDo: Check the entries within the section are sorted too
 
 }
